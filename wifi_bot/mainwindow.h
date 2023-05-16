258f6aa8948ad83a255cd18ef27537be69471ee1
G3R74W <83832635+G3R74W@users.noreply.github.com>
2023-05-16 15:35:10 +0200

    Add files via upload

---
+++ wifi_bot/mainwindow.h
@@ -0,0 +1,38 @@
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "myrobot.h"
+
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_connect_clicked();
+
+    void on_disconnect_clicked();
+
+    void on_forward_clicked();
+
+    void on_forward_speed_valueChanged(int arg1);
+
+private:
+    Ui::MainWindow *ui;
+    MyRobot wifibot;
+    int arg1 = 0;
+};
+
+#endif // MAINWINDOW_H
